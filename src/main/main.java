1e9719755f366ab85e7def6cb109dee889133c6f
Fernando Díaz <111396436+xjf255@users.noreply.github.com>
2024-04-07 20:07:10 -0600

    Add files via upload

---
+++ src/main/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import views.LoginView;
+
+/**
+ *
+ * @author fernando
+ */
+public class main {
+    public static void main(String[] args) {
+        //instancia del login
+        LoginView login = new LoginView();
+        login.setVisible(true);
+    }
+}
